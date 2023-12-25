9b71b5724417553c68efbbf670aba432bca9b599
JY LEE <wndyd0110@gmail.com>
2023-12-25 18:47:16 +0900

    카테고리 유효성 검사 구현 완료

--- src/main/java/org/fktm/fastpickup/category/dto/CategoryDTO.java
+++ src/main/java/org/fktm/fastpickup/category/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package org.fktm.fastpickup.category.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +16,8 @@ import lombok.extern.log4j.Log4j2;
 public class CategoryDTO {
 
     private int cno;             // 카테고리 번호
+
+    @NotBlank(message = "카테고리명은 필수 입력란 입니다.")
     private String categoryName; // 카테고리 명
     private boolean isDeleted;   // 삭제여부
 }
