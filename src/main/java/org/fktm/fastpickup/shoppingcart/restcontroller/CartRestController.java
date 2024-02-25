e2332f1f2303673d60f7bda9023c5e65e301928a
JY LEE <wndyd0110@gmail.com>
2024-02-25 19:20:52 +0900

    장바구니 삭제 구현 완료

--- src/main/java/org/fktm/fastpickup/shoppingcart/restcontroller/CartRestController.java
+++ src/main/java/org/fktm/fastpickup/shoppingcart/restcontroller/CartRestController.java
@@ -14,6 +14,7 @@ import org.fktm.fastpickup.util.page.PageResponseDTO;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -57,6 +58,19 @@ public class CartRestController {
         PageResponseDTO<CartListDTO> list = cartService.getCartList(pageRequestDTO, memberID);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(list);
+    }
+
+    // 장바구니 삭제
+    @DeleteMapping("/remove/{cno}")
+    public ResponseEntity<Map<String, Long>> removeCartItem(
+        @PathVariable("cno") Long cno
+    ){
+        log.info("===== /api/cart/remove | DELETE  =====");
+
+        cartService.removeCartItem(cno);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(Map.of("SuccessRemoveCartItem",cno));
+
     }
 
 }
