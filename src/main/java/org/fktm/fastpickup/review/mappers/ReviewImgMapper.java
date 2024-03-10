d2c81e4910bd95bf4ad8dfb26f5b8bec771e271e
JY LEE <wndyd0110@gmail.com>
2024-03-10 18:23:17 +0900

    리뷰 이미지 매퍼 구현

--- src/main/java/org/fktm/fastpickup/review/mappers/ReviewImgMapper.java
+++ src/main/java/org/fktm/fastpickup/review/mappers/ReviewImgMapper.java
@@ -1,5 +1,15 @@
 package org.fktm.fastpickup.review.mappers;
 
+import java.util.List;
+import java.util.Map;
+
 public interface ReviewImgMapper {
+
+        // 이미지 등록 매퍼
+    // 인자 값은 상품 등록 시 List형식(이미지가 여러개)으로 imgName, UUID, rno, ord등 을 Map 형식(key : value)로 받아온다. 
+    int registReviewImg(List<Map<String, String>> imgList);
+
+    // int removeImg()
+    int removeReviewImg(Long rno);
     
 }
