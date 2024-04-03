dc97fb526ff9f35570788d5a4f9d4a85e5152056
JY LEE <wndyd0110@gmail.com>
2024-04-03 16:30:51 +0900

    Reverse Proxy 테스트

--- src/main/java/org/fktm/fastpickup/restcontroller/HealthCheckRestController.java
+++ src/main/java/org/fktm/fastpickup/restcontroller/HealthCheckRestController.java
@@ -6,9 +6,11 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthCheckRestController {
     
     @Value("${server.env}")
