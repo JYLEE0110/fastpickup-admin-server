93b1fd824dbdbd41f5586f726fbdae4ca846781b
JY LEE <wndyd0110@gmail.com>
2024-04-23 22:42:07 +0900

    S3 버킷 테스트

--- src/main/java/org/fktm/fastpickup/util/img/FileUploadController.java
+++ src/main/java/org/fktm/fastpickup/util/img/FileUploadController.java
@@ -1,51 +1,51 @@
-package org.fktm.fastpickup.util.img;
+// package org.fktm.fastpickup.util.img;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.core.io.Resource;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+// import lombok.RequiredArgsConstructor;
+// import lombok.extern.log4j.Log4j2;
+// import org.springframework.core.io.Resource;
+// import org.springframework.http.ResponseEntity;
+// import org.springframework.web.bind.annotation.*;
+// import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.Map;
+// import java.util.List;
+// import java.util.Map;
 
-@RestController
-@RequestMapping("/api/file")
-@RequiredArgsConstructor
-@Log4j2
-public class FileUploadController {
+// @RestController
+// @RequestMapping("/api/file")
+// @RequiredArgsConstructor
+// @Log4j2
+// public class FileUploadController {
 
-    private final CustomFileUtil fileUtil;
+//     private final CustomFileUtil fileUtil;
 
-    @PostMapping("/upload")
-    public List<ImgFileUploadDTO> uploadFiles (@RequestParam("files") List<MultipartFile> files ) {
+//     @PostMapping("/upload")
+//     public List<ImgFileUploadDTO> uploadFiles (@RequestParam("files") List<MultipartFile> files ) {
 
-        List<ImgFileUploadDTO> fileList = fileUtil.saveFiles(files);
+//         List<ImgFileUploadDTO> fileList = fileUtil.saveFiles(files);
 
-        log.info("------------------------------");
-        log.info("------------------------------");
-        log.info("------------------------------");
-        log.info(fileList);
+//         log.info("------------------------------");
+//         log.info("------------------------------");
+//         log.info("------------------------------");
+//         log.info(fileList);
 
-        return fileList;
-    }
+//         return fileList;
+//     }
 
-    @GetMapping("/view/{fileName}")
-    public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
+//     @GetMapping("/view/{fileName}")
+//     public ResponseEntity<Resource> viewFileGET(@PathVariable String fileName){
 
-        return fileUtil.getFile(fileName);
+//         return fileUtil.getFile(fileName);
 
-    }
+//     }
 
-    @DeleteMapping("remove/{fileName}")
-    public Map<String, String> removeFile(
-            @PathVariable("fileName") String fileName) {
+//     @DeleteMapping("remove/{fileName}")
+//     public Map<String, String> removeFile(
+//             @PathVariable("fileName") String fileName) {
 
-                fileUtil.deleteFiles(fileName);
+//                 fileUtil.deleteFiles(fileName);
 
-                return Map.of("result","success");
+//                 return Map.of("result","success");
 
-            }
-}
+//             }
+// }
 
